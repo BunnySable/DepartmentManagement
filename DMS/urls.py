46a03fba2fe6f5551b0fe6973b4f62090805d644
unknown <ganeshsable247@gmail.com>
2025-01-08 22:01:01 +0530

    second commit

--- DMS/urls.py
+++ DMS/urls.py
@@ -5,9 +5,16 @@ from DMS import views
 
 urlpatterns = [
     path('',views.showHome),
+    # path('modifydepart',views.modifyDepart),
     path('adddepart',views.addDepart),
     path('delete/<int:departid>',views.Deletedepart),
     path('update/<int:departid>',views.Updatedepart),
+
+      #Roles:
+    path('viewrole',views.viewRole),
+    path('addrole',views.addRole),
+    path('deleterole/<int:roleid>',views.Deleterole),
+    path('updaterole/<int:roleid>',views.Updaterole),
 
 
 ]
